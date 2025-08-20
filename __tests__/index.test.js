8cb2d41e604878a743be8f6057c42b26bb407dce
Shreeja Srivastava <shreejasrivastava@mac.aeriescorp.com>
2025-08-20 16:11:50 +0530

    change

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../index.js";
 
 describe("GET /", () => {
-  it("should return Hello there 👀", async () => {
+  it("should return Hello there", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("Hello there 👀");
